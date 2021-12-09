401762a9ef8df8f553c79af1455d8260d74fe2ff
Alexei Garban <garban.valdeon@gmail.com>
2021-12-09 13:57:13 +0100

    change on settings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  distDir: 'build',
     images: {
       domains: ['platformable.com', 'dl.airtable.com','https://dl.airtable.com/.attachmentThumbnails/'],
     },
-    target: 'serverless'
   }
