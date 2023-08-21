2da71e7d7febb8a003858dc025673e3b5ff737b6
leoncan122 <leon.can122@gmail.com>
2023-08-21 10:41:47 +0200

    Fix index keys

--- components/AppliedFiltersLabels.js
+++ components/AppliedFiltersLabels.js
@@ -17,20 +17,20 @@ const AppliedFiltersLabels = () => {
     <>
     {anySelection && (
         <div className="grid grid-rows-auto lg:grid-cols-5 px-10 mb-10 gap-x-3 md:gap-5">
-      {Object.entries(typeOfValues).filter(([key, value]) => key === 'All' ? false : value === true).map(array => (
-        <div className="bg-orange py-1 px-3 flex justify-between items-center gap-3 lg:gap-5 rounded-r-2xl rounded-l text-white" onClick={() => deleteFilter('typeOfValues',array[0])}>
+      {Object.entries(typeOfValues).filter(([key, value]) => key === 'All' ? false : value === true).map(array, index => (
+        <div key={index} className="bg-orange py-1 px-3 flex justify-between items-center gap-3 lg:gap-5 rounded-r-2xl rounded-l text-white" onClick={() => deleteFilter('typeOfValues',array[0])}>
             {array[0]}
             <span className="uppercase">X</span>
         </div>
       ))}
-      {Object.entries(selectedRegion).filter(([key, value]) => key === 'All' ? false : value === true).map(array => (
-        <div className="bg-pink-400 py-1 px-3 flex justify-between gap-3 lg:gap-5 rounded-r-2xl rounded-l" onClick={() => deleteFilter('selectedRegion',array[0])}>
+      {Object.entries(selectedRegion).filter(([key, value]) => key === 'All' ? false : value === true).map(array, index => (
+        <div key={index} className="bg-pink-400 py-1 px-3 flex justify-between gap-3 lg:gap-5 rounded-r-2xl rounded-l" onClick={() => deleteFilter('selectedRegion',array[0])}>
             {array[0]}
             <span className="uppercase">X</span>
         </div>
       ))}
-      {Object.entries(selectedBeneficiaryId).filter(([key, value]) => key === 'All' ? false : value.isSelected === true).map(array => (
-        <div className="bg-yellow py-1 px-3 flex justify-between gap-3 lg:gap-5 rounded-r-2xl rounded-l" onClick={() => 
+      {Object.entries(selectedBeneficiaryId).filter(([key, value]) => key === 'All' ? false : value.isSelected === true).map(array, index => (
+        <div key={index} className="bg-yellow py-1 px-3 flex justify-between gap-3 lg:gap-5 rounded-r-2xl rounded-l" onClick={() => 
           //Different deletion process than other because state is different structure  
           setUser(prev => ({...prev, selectedBeneficiaryId: {...prev['selectedBeneficiaryId'], [array[0]]: !prev['selectedBeneficiaryId'][array[0]] }}))
         }>
