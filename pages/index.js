23cfa57297f47c8dc26a4bcd86e4dc0431f797e5
leoncan122 <leon.can122@gmail.com>
2023-09-29 11:00:45 +0200

    Add robots and clean

--- pages/index.js
+++ pages/index.js
@@ -8,24 +8,17 @@ import Filters from "../components/Filters";
 import Hero from "../components/Hero";
 
 const Home = ({ data, pagination }) => {
-  // console.log("data", data)
+  console.log("data", data);
   const [user, setUser] = useContext(ValueContext);
 
   const [filteredData, setFilteredData] = useState(data.records);
   const [clientOffset, setClientOffset] = useState(data.offset);
-  const { selectedTypeOfValue, selectedRegion, typeOfValues, selectedBeneficiaryId, favorites } = user;
+  const { selectedRegion, typeOfValues, selectedBeneficiaryId } = user;
   // const router = useRouter();
   // const routerLocation = router.asPath;
   // const [loading, setLoading] = useState(false);
   const [selectedBeneficiary, setSelectedBeneficiary] = useState("All");
   const [beneficiaryId, setBeneficiaryId] = useState("All");
-
-  // const handleValuesList = () => {
-  //   setValuesList(!openValuesList);
-  //   typeof window !== undefined &&
-  //     window.localStorage.setItem("value", selectedTypeOfValue);
-  // };
-
 
   /* Get FUNCTIONS */
   const handleBeneficiary = (beneficiary) => {
@@ -50,41 +43,58 @@ const Home = ({ data, pagination }) => {
         }
       )
         .then((response) => response.json())
-        .then(res => res.records.map(beneficiary => ({ [beneficiary.fields.Name]: {id: beneficiary.id, isSelected: false} })))
-        .then((ids) => setUser(prev => ({...prev, selectedBeneficiaryId: Object.assign({}, prev.selectedBeneficiaryId, ...ids)}) ))
+        .then((res) =>
+          res.records.map((beneficiary) => ({
+            [beneficiary.fields.Name]: {
+              id: beneficiary.id,
+              isSelected: false,
+            },
+          }))
+        )
+        .then((ids) =>
+          setUser((prev) => ({
+            ...prev,
+            selectedBeneficiaryId: Object.assign(
+              {},
+              prev.selectedBeneficiaryId,
+              ...ids
+            ),
+          }))
+        );
     }
 
     getBeneficiary();
   }, []);
 
-  
-
   return (
     <Layout>
       <Head>
         {/* <title>Platformable Value Generated Tool</title> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Platformable Value Generated Tool" />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero />
       <section className="flex container mx-auto">
-      <Filters
-        data={data}
-        setFilteredData={setFilteredData}
-        selectedBeneficiary={selectedBeneficiary}
-        handleBeneficiary={handleBeneficiary}
-      />
+        <Filters
+          data={data}
+          setFilteredData={setFilteredData}
+          selectedBeneficiary={selectedBeneficiary}
+          handleBeneficiary={handleBeneficiary}
+        />
 
-      {data && (
-        <div className="flex flex-col">
-          <ToolsResults
-            typeOfValues={typeOfValues}
-            content={filteredData}
-            selectedRegion={selectedRegion}
-            clientOffset={clientOffset}
-            pagination={pagination}
-          />
-        </div>
-      )}
+        {data && (
+          <div className="flex flex-col">
+            <ToolsResults
+              typeOfValues={typeOfValues}
+              content={filteredData}
+              selectedRegion={selectedRegion}
+              clientOffset={clientOffset}
+              pagination={pagination}
+            />
+          </div>
+        )}
       </section>
       <div className="footer-top-bar h-12"></div>
     </Layout>
@@ -93,12 +103,10 @@ const Home = ({ data, pagination }) => {
 
 export default Home;
 
-
-
 export async function getServerSideProps(context) {
   const offset = "" || (await context.query.clientOffset);
 
-
+  // console.log(offset)
   const url =
     offset === undefined
       ? "https://api.airtable.com/v0/appHMNZpRfMeHIZGc/Value%20Generated"
