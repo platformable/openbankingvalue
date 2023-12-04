ef945fe68b913c3b597e69c503e8cd8c7291ab7d
leoncan122 <leon.can122@gmail.com>
2023-12-04 17:16:10 +0100

    add new relic

---
+++ pages/_document.js
@@ -0,0 +1,66 @@
+import newrelic from "newrelic";
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+
+// import { logger } from "../components/Logger";
+
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx
+  ){
+    const initialProps = await Document.getInitialProps(ctx);
+
+    /**
+     * For SSG pages the build is faster than the agent connect cycle
+     * In those cases, let's wait for the agent to connect before getting
+     * the browser agent script.
+     */
+    if (!newrelic.agent.collector.isConnected()) {
+      await new Promise((resolve) => {
+        newrelic.agent.on("connected", resolve);
+      });
+    }
+
+    const browserTimingHeader = newrelic.getBrowserTimingHeader({
+      hasToRemoveScriptWrapper: true,
+      allowTransactionlessInjection: true,
+    });
+
+    // logger.info("NextJs New Relic redirecting to a page", {
+    //   application: "NextJs NewRelic app logging",
+    //   test: "Testing logging with Winston",
+    //   pathname: ctx.pathname,
+    // });
+
+    return {
+      ...initialProps,
+      browserTimingHeader,
+    };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{ __html: this.props.browserTimingHeader }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
